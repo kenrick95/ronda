09de4de6184c617856c6bcf91374df661fe92fdf
ivanlanin <ivanlanin@74a92044-64f5-9218-544a-1d6323d921cd>
2011-03-10 06:22:23 +0000

    * BARU: Pengalihan dimiringkan * BARU: Pengembalian dicoret * BARU: Suntingan tunda

--- trunk/index.php
+++ trunk/index.php
@@ -4,12 +4,10 @@
  */
 include_once('class.ronda.php');
 $ronda = new ronda();
-$ronda->rc($_GET);
+$ronda->process($_GET);
 $ret = $ronda->html();
-
-$TITLE = 'Ronda';
-$CONTENT .= sprintf('<h1>%1$s</h1>', $TITLE);
-$CONTENT .= $ret;
+$TITLE = $ronda->title;
+$CONTENT = $ret;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
