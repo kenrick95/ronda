d2199d28f95e641e498b618e790288219a81782f
ivanlanin <ivanlanin@74a92044-64f5-9218-544a-1d6323d921cd>
2011-03-11 16:34:35 +0000

    Kembalikan $default_limit = 500

--- class.ronda.php
+++ class.ronda.php
@@ -32,7 +32,7 @@ class ronda
 	var $page_url;
 	var $idx_url;
 	var $api_url;
-	var $default_limit = 10;
+	var $default_limit = 500;
 	var $default_ns = '0|1|2|4|5|6|7|8|9|10|11|12|13|14|15|100|101';
 	var $max_limit = 500;
 	var $min_limit = 1;
