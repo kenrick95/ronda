60359099f50041f6ef9c03a1901d6374d7f9bf94
ivanlanin <ivanlanin@74a92044-64f5-9218-544a-1d6323d921cd>
2011-03-13 16:50:05 +0000

    Tampilan detail info halaman

--- trunk/index.php
+++ trunk/index.php
@@ -11,7 +11,7 @@ $TITLE = $ronda->title;
 $CONTENT = $ret;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="id" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title><?php echo($TITLE); ?></title>
